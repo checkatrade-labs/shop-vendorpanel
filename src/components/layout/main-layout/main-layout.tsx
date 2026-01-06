9ed4cc111251297d5b508685fa4f2ad196599b05
Luka Giorgadze <luka.giorgadze@gmail.com>
2026-01-06 15:53:34 -0500

    update copy and logo

--- src/components/layout/main-layout/main-layout.tsx
+++ src/components/layout/main-layout/main-layout.tsx
@@ -75,7 +75,7 @@ const Header = () => {
     <div className="w-full p-3 p-0.5 pr-2 bg-ui-bg-subtle grid w-full grid-cols-[24px_1fr_15px] items-center gap-x-3">
       {fallback ? (
         <div className="w-7 h-7">
-          <ImageAvatar src={"/logo.webp"} size={7} rounded />
+          <ImageAvatar src={"/logo-cat.svg"} size={7} rounded />
         </div>
       ) : (
         <Skeleton className="h-6 w-6 rounded-md" />
